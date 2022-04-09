9912c1bad96d354bf29ddb57ef3bdc1f1624d5b3
金禅 <mark.ck@alibaba-inc.com>
2022-04-10 06:09:56 +0800

    feat: add antd scenarios

---
+++ src/scenarios/basic-antd/index.ts
@@ -0,0 +1,37 @@
+import { init, plugins } from '@alilc/lowcode-engine';
+import registerPlugins from './plugin';
+import { scenarioSwitcher } from '../../sample-plugins/scenario-switcher';
+import '../../universal/global.scss';
+
+const preference = new Map();
+preference.set('DataSourcePane', {
+  importPlugins: [],
+  dataSourceTypes: [
+    {
+      type: 'fetch',
+    },
+    {
+      type: 'jsonp',
+    }
+  ]
+});
+
+(async function main() {
+  await plugins.register(scenarioSwitcher);
+  await registerPlugins();
+
+  init(document.getElementById('lce-container')!, {
+    // designMode: 'live',
+    // locale: 'zh-CN',
+    enableCondition: true,
+    enableCanvasLock: true,
+    // 默认绑定变量
+    supportVariableGlobally: true,
+    // simulatorUrl 在当 engine-core.js 同一个父路径下时是不需要配置的！！！
+    // 这里因为用的是 alifd cdn，在不同 npm 包，engine-core.js 和 react-simulator-renderer.js 是不同路径
+    simulatorUrl: [
+      'https://alifd.alicdn.com/npm/@alilc/lowcode-react-simulator-renderer@latest/dist/css/react-simulator-renderer.css',
+      'https://alifd.alicdn.com/npm/@alilc/lowcode-react-simulator-renderer@latest/dist/js/react-simulator-renderer.js'
+    ]
+  }, preference);
+})();
