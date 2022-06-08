4eb4804323e7c6efc984656d04024cdf0c948f65
bingblog <abingblog@gmail.com>
2022-06-08 16:47:57 +0800

    chore: 解决预览的模拟器渲染库和编辑态的引擎库版本不一致的问题

--- src/scenarios/basic-antd/index.ts
+++ src/scenarios/basic-antd/index.ts
@@ -28,12 +28,6 @@ preference.set('DataSourcePane', {
     enableCanvasLock: true,
     // 默认绑定变量
     supportVariableGlobally: true,
-    // simulatorUrl 在当 engine-core.js 同一个父路径下时是不需要配置的！！！
-    // 这里因为用的是 alifd cdn，在不同 npm 包，engine-core.js 和 react-simulator-renderer.js 是不同路径
-    simulatorUrl: [
-      'https://alifd.alicdn.com/npm/@alilc/lowcode-react-simulator-renderer@latest/dist/css/react-simulator-renderer.css',
-      'https://alifd.alicdn.com/npm/@alilc/lowcode-react-simulator-renderer@latest/dist/js/react-simulator-renderer.js'
-    ],
     requestHandlersMap: {
       fetch: createFetchHandler()
     }
