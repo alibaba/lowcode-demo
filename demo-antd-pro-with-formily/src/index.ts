b4f728c1e366bd4816a42952e7cb3f0d1522821a
JackLian <jack.lianjie@gmail.com>
2022-11-10 16:07:07 +0800

    feat: add demo-antd-pro-with-formily

---
+++ demo-antd-pro-with-formily/src/index.ts
@@ -0,0 +1,110 @@
+import { init, plugins } from '@alilc/lowcode-engine';
+import { createFetchHandler } from '@alilc/lowcode-datasource-fetch-handler'
+import EditorInitPlugin from './plugins/plugin-editor-init';
+import UndoRedoPlugin from '@alilc/lowcode-plugin-undo-redo';
+import ZhEnPlugin from '@alilc/lowcode-plugin-zh-en';
+import CodeGenPlugin from '@alilc/lowcode-plugin-code-generator';
+import DataSourcePanePlugin from '@alilc/lowcode-plugin-datasource-pane';
+import SchemaPlugin from '@alilc/lowcode-plugin-schema';
+import CodeEditorPlugin from "@alilc/lowcode-plugin-code-editor";
+import ManualPlugin from "@alilc/lowcode-plugin-manual";
+import InjectPlugin from '@alilc/lowcode-plugin-inject';
+import SimulatorResizerPlugin from '@alilc/lowcode-plugin-simulator-select';
+import ComponentPanelPlugin from './plugins/plugin-component-panel';
+import DefaultSettersRegistryPlugin from './plugins/plugin-default-setters-registry';
+import LoadIncrementalAssetsWidgetPlugin from './plugins/plugin-load-incremental-assets-widget';
+import SaveSamplePlugin from './plugins/plugin-save-sample';
+import PreviewSamplePlugin from './plugins/plugin-preview-sample';
+import CustomSetterSamplePlugin from './plugins/plugin-custom-setter-sample';
+import SetRefPropPlugin from './plugins/plugin-set-ref-prop';
+import LogoSamplePlugin from './plugins/plugin-logo-sample';
+import './global.scss';
+
+async function registerPlugins() {
+  await plugins.register(EditorInitPlugin, {
+    scenarioName: 'antd-pro-with-formily',
+    displayName: 'antd 高级组件 + formily 表单组件',
+    info: {
+      urls: [
+        {
+          key: '设计器',
+          value: 'https://github.com/alibaba/lowcode-demo/tree/main/demo-antd-pro-with-formily',
+        },
+        {
+          "key": "formily 物料",
+          "value": "https://github.com/seada-low-code/lowcode-ecology/tree/main/packages/formily-materials"
+        }
+      ],
+    },
+  });
+
+  // 设置内置 setter 和事件绑定、插件绑定面板
+  await plugins.register(DefaultSettersRegistryPlugin);
+
+  await plugins.register(LogoSamplePlugin);
+
+  await plugins.register(ComponentPanelPlugin);
+
+  await plugins.register(SchemaPlugin);
+
+  await plugins.register(ManualPlugin);
+  // 注册回退/前进
+  await plugins.register(UndoRedoPlugin);
+
+  // 注册中英文切换
+  await plugins.register(ZhEnPlugin);
+
+  await plugins.register(InjectPlugin);
+
+  await plugins.register(SetRefPropPlugin);
+
+  await plugins.register(SimulatorResizerPlugin);
+
+  await plugins.register(LoadIncrementalAssetsWidgetPlugin);
+
+
+  // 插件参数声明 & 传递，参考：https://www.yuque.com/lce/doc/ibh9fh#peEmG
+  await plugins.register(DataSourcePanePlugin, {
+    importPlugins: [],
+    dataSourceTypes: [
+      {
+        type: 'fetch',
+      },
+      {
+        type: 'jsonp',
+      }
+    ]
+  });
+
+  await plugins.register(CodeEditorPlugin);
+
+  // 注册出码插件
+  await plugins.register(CodeGenPlugin);
+
+  await plugins.register(SaveSamplePlugin);
+
+  await plugins.register(PreviewSamplePlugin);
+
+  await plugins.register(CustomSetterSamplePlugin);
+};
+
+(async function main() {
+  await registerPlugins();
+
+  init(document.getElementById('lce-container')!, {
+    // locale: 'zh-CN',
+    enableCondition: true,
+    enableCanvasLock: true,
+    // 默认绑定变量
+    supportVariableGlobally: true,
+    // simulatorUrl 在当 engine-core.js 同一个父路径下时是不需要配置的！！！
+    // 这里因为用的是 alifd cdn，在不同 npm 包，engine-core.js 和 react-simulator-renderer.js 是不同路径
+    simulatorUrl: [
+      'https://alifd.alicdn.com/npm/@alilc/lowcode-react-simulator-renderer@latest/dist/css/react-simulator-renderer.css',
+      'https://alifd.alicdn.com/npm/@alilc/lowcode-react-simulator-renderer@latest/dist/js/react-simulator-renderer.js'
+    ],
+    requestHandlersMap: {
+      fetch: createFetchHandler()
+    },
+  });
+})();
