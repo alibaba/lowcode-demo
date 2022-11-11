9fc1b065ceed36b37b8da130a4fe94ddd99e46ec
JackLian <jack.lianjie@gmail.com>
2022-11-11 14:34:51 +0800

    chore: 整体构建

--- build.plugin.js
+++ build.plugin.js
@@ -1,4 +1,5 @@
 const fs = require('fs-extra');
+const configuration = require('./build.json');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -17,31 +18,67 @@ module.exports = ({ onGetWebpackConfig }) => {
         fs: 'empty',
       },
     });
+    const entries = Object.keys(configuration.entry) || [];
+    entries.map((entry) => {
+      const [scenarioName, page] = entry.split('/');
+      if (page === 'index') {
+        config
+          .plugin(entry)
+          .use(HtmlWebpackPlugin, [
+            {
+              inject: false,
+              minify: false,
+              templateParameters: {
+                version,
+                scenarioName
+              },
+              template: require.resolve('./public/index.ejs'),
+              filename: `${scenarioName}/index.html`,
+            },
+          ]);
+      }
+      if (page === 'preview') {
+        config
+          .plugin(entry)
+          .use(HtmlWebpackPlugin, [
+            {
+              inject: false,
+              minify: false,
+              templateParameters: {
+                scenarioName,
+              },
+              template: require.resolve('./public/preview.html'),
+              filename: `${scenarioName}/preview.html`,
+            },
+          ]);
 
-    config
-    .plugin('demo-general/index')
-    .use(HtmlWebpackPlugin, [
-      {
-        inject: false,
-        minify: false,
-        templateParameters: {
-          version,
-        },
-        template: require.resolve('./demo-general/public/index.ejs'),
-        filename: 'index.html',
-      },
-    ]);
-    config
-      .plugin('demo-general/preview')
-      .use(HtmlWebpackPlugin, [
-        {
-          inject: false,
-          templateParameters: {
-          },
-          template: require.resolve('./demo-general/public/preview.html'),
-          filename: 'preview.html',
-        },
-      ]);
+      }
+    })
+
+    // config
+    // .plugin('demo-general/index')
+    // .use(HtmlWebpackPlugin, [
+    //   {
+    //     inject: false,
+    //     minify: false,
+    //     templateParameters: {
+    //       version,
+    //     },
+    //     template: require.resolve('./demo-general/public/index.ejs'),
+    //     filename: 'demo-general/index.html',
+    //   },
+    // ]);
+    // config
+    //   .plugin('demo-general/preview')
+    //   .use(HtmlWebpackPlugin, [
+    //     {
+    //       inject: false,
+    //       templateParameters: {
+    //       },
+    //       template: require.resolve('./demo-general/public/preview.html'),
+    //       filename: 'demo-general/preview.html',
+    //     },
+    //   ]);
 
     config.plugins.delete('hot');
     config.devServer.hot(false);
