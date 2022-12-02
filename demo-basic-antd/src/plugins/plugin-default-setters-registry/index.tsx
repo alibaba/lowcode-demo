56c19d2a0c890769c96032fef7b97c25f14f473c
JackLian <jack.lianjie@gmail.com>
2022-12-02 20:18:32 +0800

    docs: replace yuque urls

--- demo-basic-antd/src/plugins/plugin-default-setters-registry/index.tsx
+++ demo-basic-antd/src/plugins/plugin-default-setters-registry/index.tsx
@@ -7,7 +7,7 @@ const DefaultSettersRegistryPlugin = (ctx: ILowCodePluginContext) => {
     async init() {
       const { setterMap, pluginMap } = AliLowCodeEngineExt;
       const { setters, skeleton } = ctx;
-      // 注册setterMap
+      // 注册 setterMap
       setters.registerSetter(setterMap);
       // 注册插件
       // 注册事件绑定面板
